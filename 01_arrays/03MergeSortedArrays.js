64ac4c0a14b2a7462779c8be0bfee74e00f935ca
stephen ijeh <stephencode@hotmail.com>
2024-01-05 01:50:25 +0000

    Arrays

--- 01_arrays/03MergeSortedArrays.js
+++ 01_arrays/03MergeSortedArrays.js
@@ -3,10 +3,36 @@
  * 
  * Question: 88. Merge Sorted Array
  * 
+ * Time Complexity: O(m + n) :: he code uses two pointers (nums1_index and nums2_index) to iterate through the nums1 and nums2 arrays respectively, 
+ * comparing elements and placing them in the nums1 array accordingly. 
+ * The while loop runs until all elements from nums2 have been merged into nums1.
  * 
+ * Space Complexity: O(1) :: 
  * 
  */
 
 var merge = function(nums1, m, nums2, n) {
+    let arrayPointer = nums1.length -1;
+    let nums1_index = m - 1;
+    let nums2_index = nums2.length - 1;
     
+    while (nums2_index >= 0) {
+        if(nums1[nums1_index] > nums2[nums2_index]){
+            nums1[arrayPointer] = nums1[nums1_index];
+            arrayPointer--;
+            nums1_index--;
+        }else{
+            nums1[arrayPointer] = nums2[nums2_index];
+            arrayPointer--;
+            nums2_index--;
+
+        }
+    }
+
+    console.log(nums1)
+    return nums1;
+
 };
+
+const nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3;
+merge(nums1, m, nums2, n);
