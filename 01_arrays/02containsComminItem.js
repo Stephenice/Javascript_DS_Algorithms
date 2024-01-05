e146e4fa49c68d3babab9bd132a9e61a6f10a3f5
stephen ijeh <stephencode@hotmail.com>
2024-01-05 00:23:37 +0000

    Arrays_ commonItems

--- 01_arrays/02containsComminItem.js
+++ 01_arrays/02containsComminItem.js
@@ -7,18 +7,18 @@
 // Solution One: Brute force - Big O(n*m)
 // function commonItems(arr1, arr2) {
 
-//     // nested arrays
-//     for (var i = 0; i < arr1.length; i++){
-//         for (var j = 0; j < arr2.length; j++){
-//             if (arr1[i] === arr2[j]){
-//                 console.log('true', arr1[i], arr2[j]);
-//                 break;
-//             } else{
-//                 console.log('false');
-//             }
-//         }
-//     }
-// }
+    // nested arrays
+    for (var i = 0; i < arr1.length; i++){
+        for (var j = 0; j < arr2.length; j++){
+            if (arr1[i] === arr2[j]){
+                console.log('true', arr1[i], arr2[j]);
+                break;
+            } else{
+                console.log('false');
+            }
+        }
+    }
+}
 
 const arr1 = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['x', 'y', 'a'];
@@ -26,10 +26,14 @@ const arr2 = ['x', 'y', 'a'];
 commonItems(arr1, arr2);
 
 
-// Solution Two: Big O(n)
+/** -----------------Solution Two: Big O(n)
+ * Time Complexity:   for includes() is O(n) - overall time complexity of commonItems() function becomes O(n * m)
+ * Space Complexity: O(n)  because the space used in the result object is directly proportional to the number of elements in arr1.
+ * 
+ * */ 
 
 function commonItems(arr1, arr2) {
-    // convert arrays one to Object
+    
     const result = {}
 
     for (var i = 0; i < arr1.length; i++){
@@ -41,4 +45,22 @@ function commonItems(arr1, arr2) {
         }
     }
 
+}
+
+
+/** ------------Solution Three: Big O(n) and in-build Method
+ * 
+ * 
+ * 
+ * 
+ *  */ 
+function commonItems(arr1, arr2) {
+     console.log('object')
+    for(let i=0; i < arr1.length; i++){
+      console.log('object2')
+        if(arr2.includes(arr1[i])){
+            console.log('true');
+            break;
+        }
+    }
 }
