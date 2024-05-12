13673fec4781d8feea8244d2ddedc60b09bc374b
stephen ijeh <stephencode@hotmail.com>
2024-05-12 01:21:18 +0100

    firstRecurringCharacter solution

--- 02_hash_tables/firstRecurringCharacter.js
+++ 02_hash_tables/firstRecurringCharacter.js
@@ -10,6 +10,25 @@
 
 
 function firstRecurringCharacter(input){
-    const map = {}
+    let map = {}
+
+    for (let index = 0; index < input.length; index++) {
+
+        if(map[input[index]] !== undefined){
+            console.log(input[index])
+            return input[index];
+        }else{
+            
+            map[input[index]] = index;
+            console.log(map)
+        }
+          
+    }
+    
+    return undefined;
 
 }
+
+firstRecurringCharacter([2,5,1,2,3,5,1,2,4])
+firstRecurringCharacter([2,1,1,2,3,5,1,2,4])
+firstRecurringCharacter([2,3,4,5])
